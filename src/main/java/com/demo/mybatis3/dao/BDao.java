a0d3fba69ec28bc0a31189c964dacd23c89635fc
ZZW <493229916@qq.com>
2016-10-20 14:38:06 +0800

    集成自定义tk.mapper

--- src/main/java/com/demo/mybatis3/dao/BDao.java
+++ src/main/java/com/demo/mybatis3/dao/BDao.java
@@ -5,11 +5,13 @@ import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Select;
 
 import com.demo.mybatis3.domain.BEntity;
+import com.demo.mybatis3.tkMapper.InsertListOnDuplicateUpateMapper;
 import com.demo.web.vo.BEntityVo;
 
 import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
 
-public interface BDao extends Mapper<BEntity> {
+public interface BDao extends Mapper<BEntity>,MySqlMapper<BEntity>,InsertListOnDuplicateUpateMapper<BEntity> {
 
 	@Select("select * from BEntity where id =#{id}")
 	@ResultMap("com.demo.mybatis3.dao.BDao.ChildResultMap")
--- src/test/java/mybaits/BTest.java
+++ src/test/java/mybaits/BTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
+import org.apache.shiro.util.CollectionUtils;
+import org.aspectj.bridge.Message;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,6 +14,7 @@ import com.demo.mybatis3.domain.BEntity;
 import com.demo.web.vo.BEntityVo;
 
 import base.BaseTest;
+import tk.mybatis.mapper.entity.Example;
 
 public class BTest extends BaseTest {
 
@@ -39,5 +42,15 @@ public class BTest extends BaseTest {
 		bDao.insertSelective(bEntity);
 		BEntityVo vo = bDao.queryAllChild(id);
 		assertEquals("我是父数据", vo.getChild().get(0).getMessage());
+	}
+	@Test
+	public void insertList() {
+		BEntity bEntity = new BEntity();
+		bEntity.setMessage("测试数据");
+		bDao.insertListOnDuplicateUpdate(CollectionUtils.asList(bEntity));
+		Example example = new Example(BEntity.class);
+		example.createCriteria().andCondition("message = '测试数据'");
+		List<BEntity> bEntities = bDao.selectByExample(example);
+		assertEquals(1,bEntities.size());
 	}
 }
