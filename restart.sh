ed4a6ef13b0fb8f9bacbd02fb156099baf558226
ZZW <493229916@qq.com>
2016-11-03 15:44:40 +0800

    修改重启脚本

--- restart.sh
+++ restart.sh
@@ -9,8 +9,10 @@ echo -e "*********************** start MV ***********************"
 mv -bf target/springMVC-demo.war ~/apache-tomcat-7.0.69/webapps/ROOT.war
 echo -e "*********************** start RM ***********************"
 rm -rf ~/apache-tomcat-7.0.69/webapps/ROOT
-echo -e "*********************** start tomcat ***********************"
-~/apache-tomcat-7.0.69/bin/shutdown.sh
+echo -e "*********************** stop tomcat ***********************"
+#~/apache-tomcat-7.0.69/bin/shutdown.sh
+id=$(ps -ef|grep java |grep -v grep|awk '{print $2}')
+kill -9 $id
 echo -e "*********************** start tomcat ***********************"
 ~/apache-tomcat-7.0.69/bin/startup.sh
 
