c9a66f1e02f0171e3a30e5727a29202681e2384b
ZZW <493229916@qq.com>
2016-12-02 17:14:37 +0800

    修改tomcat路径

--- restart.sh
+++ restart.sh
@@ -7,14 +7,14 @@ mvn clean
 echo -e "*********************** start mvn package ***********************"
 mvn package -DskipTests
 echo -e "*********************** start MV ***********************"
-mv -bf target/springMVC-demo.war ~/apache-tomcat-7.0.72/webapps/ROOT.war
+mv -bf target/springMVC-demo.war /usr/local/tomcat/webapps/ROOT.war
 echo -e "*********************** start RM ***********************"
-rm -rf ~/apache-tomcat-7.0.72/webapps/ROOT
+rm -rf /usr/local/tomcat/webapps/ROOT
 echo -e "*********************** stop tomcat ***********************"
 #~/apache-tomcat-7.0.72/bin/shutdown.sh
 id=$(ps -ef|grep java |grep -v grep|awk '{print $2}')
 kill -9 $id
 echo -e "*********************** start tomcat ***********************"
-~/apache-tomcat-7.0.72/bin/startup.sh
+/usr/local/tomcat/bin/startup.sh
 
 service mysqld restart
