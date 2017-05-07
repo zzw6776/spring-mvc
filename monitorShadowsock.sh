7b67a3d6f133d378f0eca618b1eff4b33f0be296
ZZW <493229916@qq.com>
2017-05-07 10:33:41 +0800

    Update monitorShadowsock.sh

--- monitorShadowsock.sh
+++ monitorShadowsock.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-id=$(ps -ef|grep ssserver |grep -v grep|awk '{print $2}')
+#id=$(ps -ef|grep ssserver |grep -v grep|awk '{print $2}')
+id=$(ps -ef|grep server.py |grep -v grep|awk '{print $2}')
 if [ ! $id ]; then
         #/etc/init.d/shadowsocks restart
         /usr/local/shadowsocks/logrun.sh
