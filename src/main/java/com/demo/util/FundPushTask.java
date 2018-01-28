22b1245ed73439615442390716b1d834b65d721e
ZZW <zzw6776@gmail.com>
2018-01-28 14:03:28 +0800

    data

--- src/main/java/com/demo/util/FundPushTask.java
+++ src/main/java/com/demo/util/FundPushTask.java
@@ -64,7 +64,7 @@ public class FundPushTask {
         fundPushTask.test();
     }
 
-    @Scheduled(cron = "0/20 * * * * ?")
+    @Scheduled(cron = "0/40 * * * * ?")
     public void test() {
         if (!FIRST_RUN) {
             WeChatPushUtil.weChatPush("SCU12427T981f7b2e2ed51c827ba5ffa7f65f18d559c5dc3614d0d","开始运行",System.currentTimeMillis()+"开始运行");
@@ -74,6 +74,7 @@ public class FundPushTask {
         try {
             JSONObject jsonObject = JSON.parseObject(result);
             int intValue = jsonObject.getJSONObject("stock").getIntValue("StockState");
+            System.out.println(intValue);
             if (intValue != 34) {
                 WeChatPushUtil.weChatPush("SCU12427T981f7b2e2ed51c827ba5ffa7f65f18d559c5dc3614d0d","有货啦",System.currentTimeMillis()+"有货啦");
             }
