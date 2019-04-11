158e5bf1aedd013a7e8990958585be43548ee909
493229916@qq.com <zzw493229916>
2019-04-12 01:47:26 +0800

    123

--- src/main/java/com/demo/util/Test.java
+++ src/main/java/com/demo/util/Test.java
@@ -5,13 +5,16 @@ import java.util.*;
 public class Test {
 
     public static void main(String[] args) throws InterruptedException {
-        run();
+        System.out.println("123");
 
     }
 
     public static void run() throws InterruptedException {
-        List<String> itemId = Arrays.asList("13572","13573","13574","13574","13575","13576","33938","33940","33941","33942","33943","33944","33945","33946","33947","33948","33949","33950","33951","33952","33953","33954","33955","33956","33957","33959","33963","33965","33967","33971","33973","33974","33975","33976","33980");
+        List<String> itemId = Arrays.asList("13573","13574","13575","13576","33938","33940","33941","33942","33943","33944","33945","33946","33947","33948","33949","33950","33951","33952","33953","33954","33955","33956","33957","33959","33963","33965","33967","33971","33973","33974","33975","33976","33980");
         for (String s : itemId) {
+            double time = Math.random() * (3600000 - 1800000) + 1800000;
+            System.out.println("休息"+time);
+            Thread.sleep((long) time);
 
             hahaha(s);
         }
